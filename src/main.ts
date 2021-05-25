f4dd6a5c1916e110edc74cfb4d72323dbb68c5c6
sanger <ruidan.guo@qq.com>
2021-05-25 19:12:53 +0800

    提交代码

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router,{setupRouter} from '/@/router';
+import 'ant-design-vue/dist/antd.css';
+
+createApp(App).mount('#app')
---
+++ src/router/index.ts
@@ -0,0 +1,2 @@
+import type {RouteRecordRaw} from 'vur-router';
+inport {createRouter,createWebHistory} from 'vue-router'
